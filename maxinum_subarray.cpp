0b5fb3fa900aa3bdf475017d07405c1f0e066796
unknown <13041236085@163.com>
2022-02-03 21:51:41 +0800

    day1

--- maxinum_subarray.cpp
+++ maxinum_subarray.cpp
@@ -1,3 +1,4 @@
+## dynamic programing
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -9,5 +10,36 @@ public:
             result = max(result, max_pre);     
         }
         return result;
+    }
+};
+
+## divide and conquer algorithms
+class Solution {
+public:
+    struct status {
+        int lSum, rSum, maxSum, iSum;
+    };
+
+    status pushUp(status lStatus, status rStatus)
+    {
+        status s;
+        s.lSum = max(lStatus.lSum, lStatus.iSum+rStatus.lSum);
+        s.rSum = max(rStatus.rSum, lStatus.rSum+rStatus.iSum);
+        s.maxSum = max(max(lStatus.maxSum, rStatus.maxSum), lStatus.rSum+rStatus.lSum);
+        s.iSum = lStatus.iSum + rStatus.iSum;
+        return s;
+    }
+    status getStatus(vector<int>& nums, int l, int r)
+    {
+        if (l == r)
+            return (status) {nums[l], nums[l], nums[l], nums[l]}; 
+        
+        int m = (l+r) >> 1;
+        status lStatus = getStatus(nums, l, m);
+        status rStatus = getStatus(nums, m+1, r);
+        return pushUp(lStatus, rStatus);
+    }
+    int maxSubArray(vector<int>& nums) {
+        return getStatus(nums, 0, nums.size()-1).maxSum;
     }
 };
